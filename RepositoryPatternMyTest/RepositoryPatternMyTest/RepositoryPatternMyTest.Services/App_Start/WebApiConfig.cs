01e93b42229d173000b828cce029a2da464e8d4e
Serghei Tuzenco <serghei.tuzenco@udev.md>
2014-10-15 15:52:42 +0300

    exclude unnecessary from services

--- RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.Services/App_Start/WebApiConfig.cs
+++ RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.Services/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace RepositoryPatternMyTest.Services
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.Services/Controllers/ValuesController.cs
+++ RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.Services/Controllers/ValuesController.cs
@@ -9,29 +9,29 @@ namespace RepositoryPatternMyTest.Services.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        // GET values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET values/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST values
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT values/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE values/5
         public void Delete(int id)
         {
         }
