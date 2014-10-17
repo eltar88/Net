12ed3e2e06f6eaa8cd41e78468e0d51464b7e17d
Serghei Tuzenco <serghei.tuzenco@udev.md>
2014-10-17 17:35:44 +0300

    some files

---
+++ RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.ORM.Mapping.Nhibernate/NhibernateMappings/EmployeeMap.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+using RepositoryPatternMyTest.DomainModel;
+
+namespace RepositoryPatternMyTest.ORM.Mapping.Nhibernate.NhibernateMappings
+{
+    public class EmployeeMap : ClassMap<Employee>
+    {
+        public EmployeeMap()
+        {
+            Id(x => x.ID);
+            Component(x => x.Name);
+        }
+    }
+}
