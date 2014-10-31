d2aa2d4a663c19dc2647dccda4710b7caa0bcefa
Serghei Tuzenco <serghei.tuzenco@udev.md>
2014-10-31 16:07:18 +0200

    dal level inception

---
+++ RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.DAL/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using RepositoryPatternMyTest.DomainModel;
+using RepositoryPatternMyTest.ORM.Nhibernate;
+
+namespace RepositoryPatternMyTest.IDAL
+{
+    public class EmployeeRepository : Repository<Employee, long>, IEmployeeRepository
+    {
+        public EmployeeRepository(ISession Session) : base(Session)
+        {
+            
+        }
+    }
+}
---
+++ RepositoryPatternMyTest/RepositoryPatternMyTest/RepositoryPatternMyTest.IDAL/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using RepositoryPatternMyTest.DomainModel;
+using RepositoryPatternMyTest.Infrastructure;
+
+namespace RepositoryPatternMyTest.IDAL
+{
+    public interface IEmployeeRepository : IPersistRepository<Employee>, IReadOnlyRepository<Employee, long>
+    {
+    }
+}
